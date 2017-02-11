764b4cbe555489edda8cc6de1b6b3bf0de4d52bb
faiface <faiface@ksp.sk>
2017-02-11 13:24:55 +0100

    add comments to demo

--- examples/demo/main.go
+++ examples/demo/main.go
@@ -70,6 +70,7 @@ func run() {
 		glfw.WindowHint(glfw.ContextVersionMinor, 3)
 		glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 		glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+		glfw.WindowHint(glfw.Resizable, glfw.False)
 
 		var err error
 
@@ -83,33 +84,45 @@ func run() {
 		glhf.Init()
 	})
 
-	const (
-		positionAttr = iota
-		textureAttr
-	)
-
 	var (
+		// Here we define a vertex format of our VertexSlice. It's actually a basic slice
+		// literal.
+		//
+		// The vertex format consists of names and types of the attributes. The name is the
+		// name that the attribute is referenced by inside a shader.
 		vertexFormat = glhf.AttrFormat{
 			{Name: "position", Type: glhf.Vec2},
 			{Name: "texture", Type: glhf.Vec2},
 		}
+
+		// Here we declare some variables for later use.
 		shader  *glhf.Shader
 		texture *glhf.Texture
 		slice   *glhf.VertexSlice
 	)
 
+	// Here we load an image from a file. The loadImage function is not within the library, it
+	// just loads and returns a image.NRGBA.
 	gopherImage, err := loadImage("celebrate.png")
 	if err != nil {
 		panic(err)
 	}
 
+	// Every OpenGL call needs to be done inside the main thread.
 	mainthread.Call(func() {
 		var err error
+
+		// Here we create a shader. The second argument is the format of the uniform
+		// attributes. Since our shader has no uniform attributes, the format is empty.
 		shader, err = glhf.NewShader(vertexFormat, glhf.AttrFormat{}, vertexShader, fragmentShader)
+
+		// If the shader compilation did not go successfully, an error with a full
+		// description is returned.
 		if err != nil {
 			panic(err)
 		}
 
+		// We create a texture from the loaded image.
 		texture = glhf.NewTexture(
 			gopherImage.Bounds().Dx(),
 			gopherImage.Bounds().Dy(),
@@ -117,8 +130,20 @@ func run() {
 			gopherImage.Pix,
 		)
 
+		// And finally, we make a vertex slice, which is basically a dynamically sized
+		// vertex array. The length of the slice is 6 and the capacity is the same.
+		//
+		// The slice inherits the vertex format of the supplied shader. Also, it should
+		// only be used with that shader.
 		slice = glhf.MakeVertexSlice(shader, 6, 6)
+
+		// Before we use a slice, we need to Begin it. The same holds for all objects in
+		// GLHF.
 		slice.Begin()
+
+		// We assign data to the vertex slice. The values are in the order as in the vertex
+		// format of the slice (shader). Each two floats correspond to an attribute of type
+		// glhf.Vec2.
 		slice.SetVertexData([]float32{
 			-1, -1, 0, 1,
 			+1, -1, 1, 1,
@@ -128,6 +153,8 @@ func run() {
 			+1, +1, 1, 0,
 			-1, +1, 0, 0,
 		})
+
+		// When we're done with the slice, we End it.
 		slice.End()
 	})
 
@@ -138,8 +165,11 @@ func run() {
 				shouldQuit = true
 			}
 
+			// Clear the window.
 			glhf.Clear(1, 1, 1, 1)
 
+			// Here we Begin/End all necessary objects and finally draw the vertex
+			// slice.
 			shader.Begin()
 			texture.Begin()
 			slice.Begin()
