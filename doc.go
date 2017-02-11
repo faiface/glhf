301b520959f9e793d02c0026f6992338ed3f103b
faiface <faiface@ksp.sk>
2017-02-11 14:18:39 +0100

    clarify error handling in doc

--- doc.go
+++ doc.go
@@ -2,5 +2,6 @@
 //
 // All calls should be done from the main thread using "github.com/faiface/mainthread" package.
 //
-// This package deliberately does not handle nor report OpenGL errors, it's up to you to cause none.
+// This package deliberately does not handle nor report trivial OpenGL errors, it's up to you to
+// cause none. It does of course report errors like shader compilation error and such.
 package glhf
